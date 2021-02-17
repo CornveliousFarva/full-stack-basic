6eb811358c0783126f63d09a185d6a27e96d9485
Greg Schuman <gcschuman@hotmail.com>
2021-02-17 10:26:22 -0800

    Finished adding the CRUD features in app.js.

--- server/app.js
+++ server/app.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const dotent = require('dotenv');
-const { request } = require('express');
+const { request, response } = require('express');
 dotenv.config();
 
-
+const dbService = require('./dbService');
 
 app.use(cors());
 app.use(express.json());
@@ -14,18 +14,62 @@ app.use(express.urlencoded({ extended : false}))
 
 // Create
 app.post('/insert', (request, response) => {
-    
-})
+    const { name } = request.body;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.insertNewName(name);
+
+    result
+    .then(data => response.json({ data: data}))
+    .catch(err => console.log(err));
+});
 
 // Read
 app.get('/getAll', (request, response) => {
-    response.json({
-        success: true
-    });
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.getAllData();
+
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
 });
 
 // Update
+app.patch('/update', (request, response) => {
+    const { id, name } = request.body;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.updateNameById(id, name);
+
+    result
+    .then(data => response.json({success : data}))
+    .catch(err => console.log(err));
+})
 
 
 // Delete
+app.delete('/delete/:id', (request, response) => {
+    const { id } = request.params;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.deleteRowById(id);
+
+    result
+    .then(data => response.json({success : data}))
+    .catch(err => console.log(err));
+})
+
+app.get('/search/:name', (request, response) => {
+    const { name } = request.params;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.deleteRowByName(name);
+
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+})
+
+// Shows the port is listening
 app.listen(process.env.PORT, () => console.log('its working'))
